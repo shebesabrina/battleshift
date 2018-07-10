f3df837fa2e6b8d8708c0ea6515e38ff997e07da
zdcthomas <zdcthomas@yahoo.com>
2018-07-10 11:32:57 -0600

    adds mailcatcher dev settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,6 +10,8 @@ Rails.application.configure do
   config.eager_load = false
 
   config.action_mailer.default_url_options = { host: "localhost:3000" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 
 
   # Show full error reports.
