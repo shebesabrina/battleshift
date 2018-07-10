edf2ea7e33e45eeff2a33d3e86d13ce26f89b4f2
zdcthomas <zdcthomas@yahoo.com>
2018-07-10 11:39:24 -0600

    implements flash message in application.html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
   def create
-    user = User.create(user_params)
+    user = User.new(user_params)
     user.verified = false
-    user.save
-    session[:id] = user.id
-    VerificationMailer.verify(current_user).deliver_now
-    redirect_to dashboard_path
+    if user.save
+      session[:id] = user.id
+      VerificationMailer.verify(current_user).deliver_now
+      redirect_to dashboard_path
+    else
+      flash[:error] = "Invalid information"
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   private
