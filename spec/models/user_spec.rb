939402f10125d99fb25de8b03e13d0070746f9c5
zdcthomas <zdcthomas@yahoo.com>
2018-07-10 09:28:06 -0600

    TESTS alphnumericness of api token

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,16 @@ RSpec.describe User, type: :model do
   it {should validate_presence_of(:name)}
   it {should validate_presence_of(:email)}
   it {should validate_presence_of(:password_digest)}
+
+  describe "Api Token" do
+    it 'should be 32 characters long' do
+      expect(api_token.length).to be(32)
+    end
+
+    it 'should contain only alpha numerics, no special characters' do
+      expect(api_token).to match(/w/)
+      expect(api_token).to_not match(/\W/)
+
+    end
+  end
 end
