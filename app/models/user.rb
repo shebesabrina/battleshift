8568fe701f574b573e9137314c3f3d4528b8ea03
zdcthomas <zdcthomas@yahoo.com>
2018-07-10 09:21:35 -0600

    TESTS uniqueness of api token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
   validates_presence_of :name, :email, :password_digest
+  validates_uniqueness_of :api_token, :verification_token
 
   def confirmation_token
     if verification_token.blank?
       verification_token = SecureRandom.urlsafe_base64.to_s
     end
   end
+
+
 end
