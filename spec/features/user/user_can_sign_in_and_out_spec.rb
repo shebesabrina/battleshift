7a1c65c15f128b5b6d56e78ff769e3266b585fdf
Sabrina Robinson <sabrinarob@gmail.com>
2018-07-11 17:32:26 -0600

    ATP - User can login and logout successfully

--- spec/features/user/user_can_sign_in_and_out_spec.rb
+++ spec/features/user/user_can_sign_in_and_out_spec.rb
@@ -2,30 +2,27 @@ require 'rails_helper'
 
 describe 'Registered User' do
   it 'should be able to login' do
+    user = create(:user)
     visit '/'
 
     click_on 'Log in'
 # save_and_open_page
-    email = 'something@gmail.com'
-    password =  'password'
 
-    fill_in :user_email, with: email
-    fill_in :user_password, with: password
+    fill_in :user_email, with: user.email
+    fill_in :user_password, with: user.password
     click_on 'Ready to Battle!'
 
     expect(current_path).to eq(dashboard_path)
   end
 
   it 'should be able to logout' do
+    user = create(:user)
     visit '/'
 
     click_on 'Log in'
 
-    email = 'something@gmail.com'
-    password =  'password'
-
-    fill_in :user_email, with: email
-    fill_in :user_password, with: password
+    fill_in :user_email, with: user.email
+    fill_in :user_password, with: user.password
     click_on 'Ready to Battle!'
 
     click_on 'Log out'
