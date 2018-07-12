027b5d07a86c5c5a4b0d4e7460945db9b4fa2307
Sabrina Robinson <sabrinarob@gmail.com>
2018-07-11 18:06:51 -0600

    ATP - resolved registration process to require a password confirmation to register

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,15 @@ class User < ApplicationRecord
   has_secure_password
   validates_presence_of :name, :email, :password_digest
   validates_uniqueness_of :api_token, :verification_token
+  validates_confirmation_of :password_confirmation
 
   def confirmation_token
     if verification_token.nil?
       self.verification_token = SecureRandom.urlsafe_base64.to_s
     end
   end
-  
-  
+
+
   # def api_token_creation
   #   # if api_token.nil?
   #   #   self.api_token = SecureRandom.urlsafe_base64(30).to_s
