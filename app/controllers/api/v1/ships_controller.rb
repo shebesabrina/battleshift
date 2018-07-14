f09cd0cb73caf9d49da40c26221eb278571690f0
Sabrina Robinson <sabrinarob@gmail.com>
2018-07-14 16:24:44 -0600

    Finds game id and ship params to be placed

--- app/controllers/api/v1/ships_controller.rb
+++ app/controllers/api/v1/ships_controller.rb
@@ -1,6 +1,17 @@
 class Api::V1::ShipsController < ApiController
 
   def create
-
+    #check to see if ship was placed Successfully
+      if board.run
+      # @user.games.first.player_1_board.board[0].first["A1"]
+      # ship_spaces = @user.games.find(params["game_id"]).player_1_board.get_spaces_between(params["start_space"], params["end_space"])
+      ship = Ship.new(params["ship_size"])
+      placer = ShipPlacer.new(@user.games.find(params["game_id"]).player_1_board, ship, params["start_space"], params["end_space"]).run
+      # binding.pry
+      render json: @user.games.find(params["game_id"]), status: 200, message: "Successfully placed ship with a size of #{params["ship_size"]}. You have 1 ship(s) to place with a size of #{ship}."
+      else
+      render json: #{body: "You're done. Ships have been placed."} status: :bad_request if ships have all been placed whether it's a 2 ship or 3 ship length
+      #raise the error message in the ship_placer.
+    end
   end
 end
