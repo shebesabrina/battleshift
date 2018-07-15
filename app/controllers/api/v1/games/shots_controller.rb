b15e313fecb621a6fca2db5be335e8c6e62869ec
zdcthomas <zdcthomas@yahoo.com>
2018-07-15 14:18:18 -0600

    implements turn checking

--- app/controllers/api/v1/games/shots_controller.rb
+++ app/controllers/api/v1/games/shots_controller.rb
@@ -1,9 +1,19 @@
 class Api::V1::Games::ShotsController < ApiController
+  before_action :check_turn
   def create
-    game = Game.find(params[:game_id])
+   
 
-    turn_processor = TurnProcessor.new(game, params[:shot][:target])
+    turn_processor = TurnProcessor.new(@game, params[:shot][:target])
     turn_processor.run!
-    render json: game, message: turn_processor.message
+    render json: @game, message: turn_processor.message
   end
+
+  def check_turn
+    @game = Game.find(params[:game_id])
+    if @game.current_turn == "player_1" && @game.player_1 == @user || @game.current_turn == "player_2" && @game.player_2 == @user
+    else
+      render json: @game, status: 400, message: "Invalid move. It's your opponent's turn"
+    end
+  end
+
 end
