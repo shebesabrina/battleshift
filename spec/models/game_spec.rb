cd2f006de3014c3eab4ac0053b393c0380e4fbe0
zdcthomas <zdcthomas@yahoo.com>
2018-07-15 23:50:08 -0600

    removes auto generated tests

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
   context 'instance methods' do
     let(:player_1_board)   { Board.new(4) }
     let(:player_2_board)   { Board.new(4) }
